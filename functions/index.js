4daaa2c2dfba2d3924113f6bccb717b380d94df2
Michael Zhao <michaelzhao314@gmail.com>
2021-11-14 03:02:45 -0600

    Added working checkName function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.checkName = functions.https.onRequest((request, response) => {
+    var db = admin.firestore();
+
+    db.collection('names')
+        .add({
+            first: 'William',
+            last: 'Jetson',
+            born: 1978,
+        })
+        .then((docRef) => {
+            functions.logger.info('Document written with ID: ', docRef.id);
+        })
+        .catch((error) => {
+            functions.logger.error('Error adding document: ', error);
+        });
+
+    db.collection('names')
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                functions.logger.info(`${doc.id} => ${doc.data()}`);
+            });
+        });
+});
