5419076a9c8aa22f8e5254c0ad7cd1742b82e876
Michael Zhao <michaelzhao314@gmail.com>
2021-11-13 18:28:25 -0600

    Added MUI framework

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#f01716',
+        },
+        secondary: {
+            main: '#f7f0e4',
+        },
+    },
+});
+
+export default theme;
